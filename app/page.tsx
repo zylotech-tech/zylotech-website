4484fe656a1ee3fba72dec1e34437ad61ec42f97
zylotech-tech <zylo.tech.ai@gmail.com>
2026-07-10 17:38:08 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import Hero from "@/components/sections/hero";
 import Services from "@/components/sections/services";
-import Navbar from "@/components/layout/navbar";
+import Projects from "@/components/sections/projects";
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <Hero />
-        <Services />
-      </main>
-    </>
+    <main>
+      <Hero />
+      <Services />
+      <Projects />
+    </main>
   );
 }
