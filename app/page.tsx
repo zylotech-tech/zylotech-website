ea1e6647a840ffd8617da924100cc3296a4d470e
zylotech-tech <zylo.tech.ai@gmail.com>
2026-07-12 10:44:54 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/sections/hero";
 import Services from "@/components/sections/services";
 import Projects from "@/components/sections/projects";
+import Contact from "@/components/sections/contact";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Services />
       <Projects />
+      <Contact />
     </main>
   );
 }
